cbf85b0de21f6758f7e8c0118194e043c13baa67
Sarah Ohlin <sarahohlin1@gmail.com>
2019-12-27 14:57:25 -0500

    minor layout changes

--- frontend/App/Home.tsx
+++ frontend/App/Home.tsx
@@ -1,6 +1,6 @@
 import React, { SFC } from 'react';
 import { AppBar, Toolbar, Typography, Grid, IconButton } from '@material-ui/core';
-import { Brightness4 } from '@material-ui/icons'
+import { Brightness4Outlined as Sun } from '@material-ui/icons'
 
 export interface HomeProps {
 	toggleTheme: () => void;
@@ -13,14 +13,13 @@ const Home: SFC<HomeProps> = (props) => {
 		<AppBar position="static">
 			<Toolbar>
 				<Grid container justify="flex-start">
-					<Typography component="p" variant="h5" color="secondary">OwO</Typography> 
 				</Grid>
 				<Grid container justify="center">
 
 				</Grid>
 				<Grid container justify="flex-end">
 					<Grid item>
-						<IconButton onClick={props.toggleTheme}><Brightness4 /></IconButton>
+						<IconButton onClick={props.toggleTheme}><Sun style={{ fontSize: 32 }} /></IconButton>
 					</Grid>
 				</Grid>
 			</Toolbar>
