2d08843693c207f19f55d1a77fc6b17f67ad08a7
Sarah Ohlin <sarahohlin1@gmail.com>
2019-12-27 14:56:59 -0500

    added expres-session

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 require('dotenv').config();
 import appRoot from 'app-root-path';
 import api from './api';
+import session from 'express-session';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+	secret: process.env.SECRET
+}));
 
 app.use('/public', express.static(appRoot.resolve('public')));
 app.use('/api', api);
