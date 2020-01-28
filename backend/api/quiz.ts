1d026c2f893a581ed2e377e2a096e4f8f04d0ca0
Sarah Ohlin <sarahohlin1@gmail.com>
2020-01-28 11:00:15 -0500

    feat: add quiz request validation

--- backend/api/quiz.ts
+++ backend/api/quiz.ts
@@ -2,11 +2,22 @@ import express, { Request, Response } from 'express';
 import Quiz from '../db/Models/Quiz';
 import { check, validationResult } from 'express-validator';
 import authMiddleware from '../middleware/auth';
+import { isArray } from 'util';
 
 const quiz = express.Router();
 
 quiz.post('/new', [
-	authMiddleware
+	authMiddleware,
+	check('questions').custom(qs => {
+		if (!isArray(qs)) throw 'questions param is not an array';
+		qs.forEach(q => {
+			if(!q.question) throw 'no question provided';
+			if(!isArray(q.answers) ||
+			   q.answers.length < 1) throw 'not enough answers provided';
+			if(!q.correct ||
+				q.correct.length < 1) throw 'no correct answer provided';
+		});
+	}),
 ], async (req: Request, res: Response) => {
 	return res.send('placeholder');
 });
