a5f8f1874df67633d2477101588ef69b10d9cdb9
Sarah Ohlin <sarahohlin1@gmail.com>
2020-01-28 13:03:57 -0500

    refactor: make all fields optional

--- frontend/Util/IQuestion.ts
+++ frontend/Util/IQuestion.ts
@@ -1,7 +1,7 @@
 export default interface IQuestion {
-	question: string;
-	answers: [string];
-	createdAt: Date;
-	updatedAt: Date;
-	correct: [number];
+	question?: string;
+	answers?: [string];
+	createdAt?: Date;
+	updatedAt?: Date;
+	correct?: [number];
 }
--- frontend/Util/IQuiz.ts
+++ frontend/Util/IQuiz.ts
@@ -1,7 +1,7 @@
 import IQuestion from './IQuestion';
 
 export default interface IQuiz {
-	authorID: string;
-	name: string;
-	questions: [IQuestion];
+	authorID?: string;
+	name?: string;
+	questions?: [IQuestion];
 }
